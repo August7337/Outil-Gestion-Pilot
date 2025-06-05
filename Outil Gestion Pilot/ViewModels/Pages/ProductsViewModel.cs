2e45996df4dee8bbe7064c039d2f184ecee4ec55
August7337 <augustin.tarit@gmail.com>
2025-06-05 15:21:31 +0200

    Barre de recherche code produit

--- Outil Gestion Pilot/ViewModels/Pages/ProductsViewModel.cs
+++ Outil Gestion Pilot/ViewModels/Pages/ProductsViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Outil_Gestion_Pilot.Models;
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.DirectoryServices;
+using System.Windows.Data;
 
 namespace Outil_Gestion_Pilot.ViewModels.Pages
 {
@@ -9,12 +12,45 @@ namespace Outil_Gestion_Pilot.ViewModels.Pages
         [ObservableProperty]
         private ObservableCollection<Product> products;
 
+        public ICollectionView ProductsView { get; set; }
+        [ObservableProperty]
+        private string searchCode;
+
         public ProductsViewModel()
         {
             Products = new ObservableCollection<Product>();
             LoadSampleProducts();
+
+            ProductsView = CollectionViewSource.GetDefaultView(Products);
+            ProductsView.Filter = CodeSearch;
         }
 
+        /// <summary>
+        /// Refreshes the data grid when the function is called.
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnSearchCodeChanged(string value)
+        {
+            ProductsView.Refresh();
+        }
+
+        /// <summary>
+        /// finds the products in the product collection that starts with the text of the obj.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool CodeSearch(object obj)
+        {
+            if (obj is not Product product) return false;
+            if (string.IsNullOrWhiteSpace(SearchCode))
+                return true;
+        
+            return product.Code.StartsWith(SearchCode, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Loads the data. temporary.
+        /// </summary>
         private void LoadSampleProducts()
         {
             Products.Add(new Product
